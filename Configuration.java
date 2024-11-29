cdbc99861d2e97d75c67a4cc472c8011e78325b1
Karene01 <karenerakotonindrina@gmail.com>
2024-11-29 12:27:08 +0100

    commit
    création de la classe Configuration.java dans la branche Configuration

---
+++ Configuration.java
@@ -0,0 +1,11 @@
+public class Configuration {
+
+    private final static String[][] bateaux = {
+        {"1", "Porte-avions", "5"},
+        {"2", "Cuirasse", "4"},
+        {"3", "Croiseur", "3"},
+        {"4", "Croiseur", "3"},
+        {"5", "Torpimmeur", "2"}
+    };
+    
+}
